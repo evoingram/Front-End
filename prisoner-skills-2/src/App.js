e88acc70a6f8ed032de64e29d10f1b4f327d488b
Gremlin4544 <alve@mckethen.com>
2020-02-03 14:08:57 -0500

    created homepage and loginpage - display only - NO DATA

--- prisoner-skills-2/src/App.js
+++ prisoner-skills-2/src/App.js
@@ -1,13 +1,20 @@
 import React from 'react';
+import { Route } from "react-router-dom";
 import './App.css';
 
 import Header from "./components/header/Header"
+import HomePage from "../src/pages/homepage/HomePage";
+import LoginPage from "../src/pages/login/LoginPage";
 
 function App() {
   return (
     <div className="App">
       <h1>Prisoner Skills 2</h1>
       <Header />
+        <div>
+          <Route exact path="/" component={HomePage} />
+          <Route path="/login" component={LoginPage} />
+        </div>
     </div>
   );
 }
---
+++ prisoner-skills-2/src/pages/login/LoginPage.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const LoginPage = () => (
+    <div className="login-page">
+        <p>signin goes here ..</p>
+        <p>signup goes here ..</p>
+    </div>
+);
+
+export default LoginPage;

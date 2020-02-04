27e5b035ae895771c5131269e05c9ff357c7de90
Gremlin4544 <alve@mckethen.com>
2020-02-04 16:06:26 -0500

    updated and cleaned up links, homepage is now the signup/signin page

--- prisoner-skills-2/src/App.js
+++ prisoner-skills-2/src/App.js
@@ -3,17 +3,14 @@ import { Route } from "react-router-dom";
 import './App.css';
 
 import Header from "./components/header/Header"
-import HomePage from "../src/pages/homepage/HomePage";
 import LoginPage from "../src/pages/login/LoginPage";
 
 function App() {
   return (
     <div className="App">
-      <h1>Prisoner Skills 2</h1>
       <Header />
         <div>
-          <Route exact path="/" component={HomePage} />
-          <Route path="/login" component={LoginPage} />
+          <Route exact path="/" component={LoginPage} />
         </div>
     </div>
   );
--- prisoner-skills-2/src/components/header/Header.jsx
+++ prisoner-skills-2/src/components/header/Header.jsx
@@ -1,15 +1,18 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
+import "./header.scss";
 
 const Header = () => {
    return (
-    <div className="options">
-        <Link className="option" to="/login">
-            SIGN UP
-        </Link>
-        {/* <Link className="option" >
-            CONTACT
-        </Link> */}
+    <div className="header">
+        <div className="options">
+            <Link className="option" to="/login">
+                SIGN UP
+            </Link>
+            {/* <Link className="option" >
+                CONTACT
+            </Link> */}
+        </div>
     </div>
    );
 };
